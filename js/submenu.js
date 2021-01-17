7ce1bfc66c90949f06dba36cf4a98fe6d5cb4fbc
RayaraSantos <74152631+RayaraSantos@users.noreply.github.com>
2021-01-17 18:25:26 -0300

    Add files via upload

---
+++ js/submenu.js
@@ -0,0 +1,13 @@
+function amenu(){
+    var i = document.getElementById("submenu");
+
+    i.style.display = "block";
+}
+
+function fmenu(){
+    var y = document.getElementById("submenu");
+
+    y.style.display = "none";
+
+
+}
